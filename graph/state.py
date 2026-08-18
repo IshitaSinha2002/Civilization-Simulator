03a121767cafe015c950c44bd8a01db88f25c96b
Ishita Sinha <ishitasinha16@gmail.com>
2026-08-18 18:02:04 +0530

    Adding state

---
+++ graph/state.py
@@ -0,0 +1,19 @@
+from typing import TypedDict, List
+
+class CivilizationState(TypedDict):
+    year: int
+
+    population: int
+    food: int
+    wealth: int
+
+    stability: int
+    military_strength: int
+
+    technology_level: int
+    infrastructure: int
+
+    current_event: str
+    event_history: List[str]
+
+    simulation_status: str
