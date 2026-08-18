9d7a5fb7891618b74e76625faaf0941b6de7cc62
Ishita Sinha <ishitasinha16@gmail.com>
2026-08-18 23:23:49 +0530

    Adding simulate year node

---
+++ graph/nodes.py
@@ -0,0 +1,35 @@
+from graph.state import CivilizationState
+
+
+def simulate_year(state: CivilizationState) -> CivilizationState:
+    population = state["population"]
+    food = state["food"]
+    wealth = state["wealth"]
+
+    # Population consumes food
+    food_consumption = max(1, population // 1000)
+
+    food -= food_consumption
+
+    # Basic population change based on food availability
+    if food > 60:
+        population_growth = max(1, population // 100)
+        population += population_growth
+
+    elif food < 30:
+        population_decline = max(1, population // 200)
+        population -= population_decline
+
+    # Economic change based on food availability
+    if food > 50:
+        wealth += 2
+    elif food < 30:
+        wealth -= 2
+
+    return {
+        **state,
+        "year": state["year"] + 1,
+        "population": max(0, population),
+        "food": max(0, food),
+        "wealth": max(0, wealth),
+    }
